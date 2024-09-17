5f3c48a78da9f55619bb864e7c0d45f9a79fc972
Andrew Villarrubia <andrew@primefour.com>
2024-09-17 18:01:08 -0500

    quotes BEGONE

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,7 +9,7 @@ export default function Page() {
                 <p className="text-lg">This is a mostly-manual effort to collect &quot;official&quot; unofficial Magic: the Gathering rules/policy rulings in a single place. You know, the stuff that #WotCStaff post on Twitter. That sort of thing.</p>
                 <p className="text-lg">From <b>Villa</b>, with love and grumpiness that this project needs to exist.</p>
                 <p className="text-lg">Am I missing something that you think should be here? Email me at judgevillawhatever@gmail.com</p>
-                <h1 className="mb-0">"Moleman"?</h1>
+                <h1 className="mb-0">&quot;Moleman&quot;?</h1>
                 <p className="text-lg">I asked someone what the opposite of a bird was and they said &quot;mole.&quot;</p>
             </section>
         </main>
