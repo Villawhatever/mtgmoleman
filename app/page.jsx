fdeafb4e3bd5ad1b53bb80a90001fc0a643211d1
Andrew Villarrubia <andrew@primefour.com>
2024-09-18 18:48:57 -0500

    Dong, where is my data?

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import data from 'data/magic.json';
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
+            {console.log(data)}
             {data.forEach(ruling => {
                     <section className="flex flex-col items-start gap-3 sm:gap-4">
                         <RulingCard ruling={ruling} />
