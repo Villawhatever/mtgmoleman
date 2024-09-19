4c3d28e212c818e7019385536742ff1e9cc858f1
Andrew Villarrubia <andrew@primefour.com>
2024-09-18 20:48:13 -0500

    Definitely not IP infringement courtesy of Travis.

--- components/header.jsx
+++ components/header.jsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import moleman from 'public/jacemoleman.png';
 
 const navItems = [
     { linkText: 'Home', href: '/' },
@@ -8,6 +10,9 @@ const navItems = [
 export function Header() {
     return (
         <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
+            <Link href="/">
+                <Image src={moleman} alt="Jace Moleman" />
+            </Link>
             {!!navItems?.length && (
                 <ul className="flex flex-wrap gap-x-4 gap-y-1">
                     {navItems.map((item, index) => (
