a51409ecea57c9e381a1db6680c03de721a880b3
Andrew Villarrubia <andrew@primefour.com>
2024-09-18 19:17:21 -0500

    Maybe now?

--- app/page.jsx
+++ app/page.jsx
@@ -1,35 +1,15 @@
 import { RulingCard } from 'components/ruling-card';
 import data from 'data/magic.json';
-import Link from 'next/link';
-import { Markdown } from 'components/markdown';
-import { RandomQuote } from 'components/random-quote';
 
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
-            {console.log(data)}
-            {data.forEach(ruling => {
-                    <h1>Yo what the fuck {ruling.from}</h1>
-                    // <section className="flex flex-col items-start gap-3 sm:gap-4">
-                    //     <RulingCard ruling={ruling} />
-                    //     <h1>{ruling.from}</h1>
-                    // </section>
-                })
-            }
             <section className="flex flex-col items-start gap-3 sm:gap-4">
-                
+                {data.forEach(ruling => {
+                        <RulingCard ruling={ruling} />
+                    })
+                }
                 <RulingCard ruling={data[0]} />
-                <h1 className="mb-0">Netlify Platform Starter - Next.js</h1>
-                <p className="text-lg">Get started with Next.js and Netlify in seconds.</p>
-                <Link
-                    href="https://docs.netlify.com/frameworks/next-js/overview/"
-                    className="btn btn-lg btn-primary sm:btn-wide"
-                >
-                    Read the Docs
-                </Link>
-            </section>
-            <section className="flex flex-col gap-4">
-                <RandomQuote />
             </section>
         </main>
     );
